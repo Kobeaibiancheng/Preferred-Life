338a0d299705fed66b6265fd0258d9e395d9c3c6
Kobeaibiancheng <wyx160413@yeah.net>
2025-05-02 20:26:43 +0800

    基于redisson实现可重入锁

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -10,6 +10,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.SimpleRedisLock;
 import com.hmdp.utils.UserHolder;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.aop.framework.AopContext;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -38,6 +40,9 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
+    @Resource
+    private RedissonClient redissonClient;
+
     @Override
     public Result seckillVoucher(Long voucherId) {
         //1.查询
@@ -61,9 +66,12 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 
 
         Long userId = UserHolder.getUser().getId();
-        //创建分布式锁对象
-        SimpleRedisLock lock = new SimpleRedisLock(stringRedisTemplate, "order" + userId);
-        boolean tryLock = lock.tryLock(5L);
+        //创建redis分布式锁对象
+        //SimpleRedisLock lock = new SimpleRedisLock(stringRedisTemplate, "order" + userId);
+
+        //创建redisson对象
+        RLock lock = redissonClient.getLock("order" + userId);
+        boolean tryLock = lock.tryLock();
         //判断锁是否获取成功
         if(!tryLock){
             //获取锁失败，不成功怎么办
