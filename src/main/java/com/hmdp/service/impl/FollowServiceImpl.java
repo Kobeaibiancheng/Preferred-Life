29abc9eb02491ed8faf944e75b00a88762137b85
Kobeaibiancheng <wyx160413@yeah.net>
2025-05-13 15:10:54 +0800

    改造关注用户的功能，将关注的用户id保存到redis中的set

--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -8,7 +8,10 @@ import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.UserHolder;
 import jdk.nashorn.internal.ir.CallNode;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -21,21 +24,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+
+
     @Override
     public Result follow(Long followUserId, Boolean isFollow) {
         //获取当前用户id
         Long userId = UserHolder.getUser().getId();
+
+        String key = "follows:" + userId;
         //判断是关注还是取关
         if (isFollow){
             //如果是关注，添加数据
             Follow follow = new Follow();
             follow.setFollowUserId(followUserId);
             follow.setUserId(userId);
-            save(follow);
+            //save(follow);原先的实现方式
+            boolean isSuccess = save(follow);
+            if (isSuccess){
+                //如果关注成功，保存到redis中的set   sadd userId followerUserId
+                stringRedisTemplate.opsForSet().add(key,followUserId.toString());
+            }
         }else{
             //如果是取关，就删除数据  delete from tb_follow where user_id = ? and follow_user_id = ?
             remove(new QueryWrapper<Follow>()
                     .eq("follow_user_id", followUserId).eq("user_id", userId));
+            //把用户关注的id从set集合中移除
+            stringRedisTemplate.opsForSet().remove(key,followUserId.toString());
         }
 
         return Result.ok();
